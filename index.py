f443f3467c385b6d39d8581c797ef86736f0954c
josh-weimer <josh@polydelta.ai>
2024-08-28 11:48:54 -0400

    styling

--- index.py
+++ index.py
@@ -14,13 +14,18 @@ from apps import dashboard, sme_annotation
 
 logo = Image.open('assets/PD Color.png')
 
+# Include the DM Sans font from Google Fonts in the external_stylesheets
+app.css.append_css({
+    "external_url": "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+})
+
 #the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "17rem",
+    "width": "20rem",
     "padding": "2rem 1rem",
     "background-color": "#000000",
 }
@@ -28,7 +33,7 @@ SIDEBAR_STYLE = {
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "18rem",
+    "margin-left": "21rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
@@ -55,8 +60,16 @@ sidebar = html.Div(
 
 content = html.Div(id="page-content", children=sme_annotation.layout, style=CONTENT_STYLE)
 
-app.layout = html.Div([dcc.Location(id="url", children='/augmented-annotation'), sidebar, content])
-
+app.layout = html.Div(
+    style={
+        "fontFamily": "DM Sans, sans-serif !important"
+    },
+    children=[
+        dcc.Location(id="url", children='/augmented-annotation'),
+        sidebar,
+        content
+    ]
+)
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
